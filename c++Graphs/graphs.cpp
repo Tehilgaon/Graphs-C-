faf1ebf68d4f47a5cd8357f3a2ed3ffca5151263
tehilgaon <TEHILA611@GMAIL.COM>
2020-01-29 12:00:01 +0200

    graph

--- c++Graphs/graphs.cpp
+++ c++Graphs/graphs.cpp
@@ -1,1 +1,76 @@
 #include "graphs.h"
+
+void Graph::addv(string v1)
+{ 
+	this->graph.insert(pair<string, Vertex*>(v1, new Vertex(v1)));
+
+}
+
+void Graph::adde(string v1, string v2)
+{
+	Vertex* ver1 =  graph.at(v1);	
+	Vertex* ver2 = graph.at(v2);
+	if (ver1 == nullptr||ver2==nullptr)
+		return;
+	auto it = ver1->getList().begin();
+	for (; it != ver1->getList().end(); it++)
+	{
+		if (it->getvDestination()->getKey() == v2)
+			return; 	 
+	}
+	ver1->setList(Edge(ver2));
+ 
+}
+
+list<Edge> Graph::vertexList(string v1)
+{
+	Vertex* ver1 = graph.at(v1);
+	if (ver1 == NULL)
+		throw exception("vertex does nor exist");
+	return ver1->getList();  
+}
+
+void Graph::printAll()
+{
+	auto it = graph.begin();
+	for (; it != graph.end(); it++)
+	{
+		cout << it->first << endl;
+		auto itList = it->second->getList().begin();
+		for (; itList != it->second->getList().end(); itList++)
+		{
+			cout << '\t' << itList->getvDestination()->getKey();
+		}
+	}
+
+}
+
+list<int> Graph::DFS()
+{
+	timer = 0;
+	list<int> finishList;
+	auto it = graph.begin();
+	for (; it != graph.end(); it++)
+	{
+		it->second->color = true;
+		it->second->pi = NULL;
+	}
+	auto it = graph.begin();
+	for (; it != graph.end(); it++)
+	{
+		if (it->second->color)
+			DFS_visit(it->second, finishList);
+	}
+	return finishList;
+ 
+}
+
+Vertex::Vertex(string key)
+{
+	 	this->Key = key;
+		this->color = true;
+		this->d = 0;
+		this->f = 0;
+		this->pi = nullptr;
+  
+}
--- c++Graphs/graphs.h
+++ c++Graphs/graphs.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include <list>
+#include <map>
 using namespace std;
 
 class Edge;
@@ -9,23 +10,46 @@ class Vertex;
 class Vertex
 {
 	string Key;
-	int color;
-	int d;
 	list<Edge> EdgeList;
-	double f;
+	
+public:
+	bool color;
+	int d; 
+	int f;
 	Vertex* pi;
+	string getKey() { return Key; }
+	list<Edge> getList() { return EdgeList; }
+	void setKey(string value) { Key = value; }
+	void setList(Edge edge) { EdgeList.push_back(edge); }
+	Vertex(string key);
 	
 
 
 };
 class Edge
 {
-	Vertex* vSource;
+	//Vertex* vSource;
 	Vertex* vDestination;
 	int distance;
+public:
+	Vertex* getvDestination() { return vDestination; }
+	Edge() {}
+	Edge(Vertex* v) { vDestination = v; distance = 1; }
 
 };
-class graphs
+class Graph
 {
+	map <string, Vertex*> graph;
+	int timer;
+public:
+	Graph() {}
+	void addv(string v1);
+	void adde(string v1, string v2);
+	list<Edge> vertexList(string v1);
+	void printAll();
+	void topologicalSort();
+	list<int> DFS();
+	void DFS_visit(Vertex* v1,list<int>);
+
 };
 
